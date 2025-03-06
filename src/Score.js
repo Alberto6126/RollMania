cedf9be0af2f79d4ae625b86c01de3e6af42a1c6
Alberto6126 <alberto.sanchez@enti.cat>
2025-03-06 14:30:48 +0100

    asdasd

--- src/Score.js
+++ src/Score.js
@@ -1,27 +1,13 @@
 import './Score.css';
-// import ScoreItem from './ScoreItem'; 
-
+import ScoreItem from './ScoreItem'; 
 
 
 function Score(){
+
     return(
         <div className="ScoreContainer">
             <ol>
-                {/* {ScoreItem}; */}
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
-                <li>ASD -- 123</li>
-                <li>ADD -- 23</li>
+                <ScoreItem />
             </ol>
         </div>
     );
--- src/ScoreItem.js
+++ src/ScoreItem.js
@@ -1,15 +1,35 @@
 import react from 'react'
 
 
-function ScoreItem(number){
-let scoreList = [];
-    for(let i = 0; i < number; i++){
-        scoreList.push(<li key={i}>Pablo --- 10</li>)
+function ScoreItem(){
+let puntos = [
+{
+    "nombre":"pablo",
+    "score":33
+},
+{
+    "nombre":"gerundio",
+    "score":13
+},
+{
+    "nombre":"miguel",
+    "score":300
+},
+{
+    "nombre":"Alberto",
+    "score":13000
+},
+
+]
+
+puntos.sort((a,b)=>b.score - a.score); //crea dos variables a y b y se les asocia temporalmente el contenido de el elemento de la array, ordena en función que a sea menor que b
+let ScoreList = [];
+    for(let i = 0; i < puntos.length; i++)
+    {
+        ScoreList.push(<li key={i}>{puntos[i].nombre}   ||      {puntos[i].score}</li>)
     }
-    
-    // scoreList.forEach((element)=>{return element});
-    console.log(scoreList);
-    return scoreList;
+    console.log(ScoreList);
+    return ScoreList;
 }
 
 export default ScoreItem;
